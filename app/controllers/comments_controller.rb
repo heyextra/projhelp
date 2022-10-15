f54ab2160f8c97700b97dcdb9fa5c610dd399943
heyextra <extrafahrali@gmail.com>
2022-10-15 17:55:24 -0400

    projhelp

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+    before_action :set_project
+
+    def create
+        @project.comments.create! params.required(:comment).permit(:content)
+        redirect_to @project
+    end
+    private
+    def set_project
+        @project = Project.find(params[:project_id])
+    end
+        
+end
